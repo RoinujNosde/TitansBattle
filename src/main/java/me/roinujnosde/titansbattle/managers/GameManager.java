44a5ea191a8fb61d0c7afc11efb6eceb896db4ad
Kawl <basssuporte@gmail.com>
2021-10-24 02:51:04 -0400

    New killer announcer in actionbar

--- src/main/java/me/roinujnosde/titansbattle/managers/GameManager.java
+++ src/main/java/me/roinujnosde/titansbattle/managers/GameManager.java
@@ -10,6 +10,8 @@ import me.roinujnosde.titansbattle.types.GameConfiguration;
 import me.roinujnosde.titansbattle.types.Scheduler;
 import me.roinujnosde.titansbattle.types.Warrior;
 import me.roinujnosde.titansbattle.utils.Helper;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -94,8 +96,12 @@ public class GameManager {
         }
         GameConfigurationDao dao = GameConfigurationDao.getInstance(plugin);
         Bukkit.getPluginManager().callEvent(new NewKillerEvent(killer, victim));
-        Bukkit.getServer().broadcastMessage(MessageFormat.format(plugin.getLang("new_killer",
-                dao.getConfigFile(gameConfig)), killer.getName()));
+        Bukkit.getServer().getOnlinePlayers().forEach((p) -> {
+            p.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(
+                    MessageFormat.format(plugin.getLang("new_killer",
+                            dao.getConfigFile(gameConfig)), killer.getName())));
+        });
+
         plugin.getDatabaseManager().getTodaysWinners().setKiller(gameConfig.getName(), killer.getUniqueId());
     }
 
