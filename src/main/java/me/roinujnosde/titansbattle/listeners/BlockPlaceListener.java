5140cd8f9b760f1089ee5760359d1097da7bc86b
Kawl <basssuporte@gmail.com>
2021-10-27 23:05:38 -0400

    block/break place during preparation time, issue 45 hint

---
+++ src/main/java/me/roinujnosde/titansbattle/listeners/BlockPlaceListener.java
@@ -0,0 +1,40 @@
+package me.roinujnosde.titansbattle.listeners;
+
+import me.roinujnosde.titansbattle.TitansBattle;
+import me.roinujnosde.titansbattle.games.Game;
+import me.roinujnosde.titansbattle.managers.DatabaseManager;
+import me.roinujnosde.titansbattle.managers.GameManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class BlockPlaceListener implements Listener {
+
+    private final TitansBattle plugin = TitansBattle.getInstance();
+    private final GameManager gm;
+    private final DatabaseManager dm;
+
+    public BlockPlaceListener() {
+        gm = plugin.getGameManager();
+        dm = plugin.getDatabaseManager();
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlace(BlockPlaceEvent event) {
+        Game game = gm.getCurrentGame().orElse(null);
+        if (game == null) {
+            return;
+        }
+
+        Player defender = event.getPlayer();
+        if (!game.isParticipant(dm.getWarrior(defender))) {
+            return;
+        }
+
+        if (!game.isInBattle(dm.getWarrior(defender))) {
+            event.setCancelled(true);
+        }
+    }
+}
