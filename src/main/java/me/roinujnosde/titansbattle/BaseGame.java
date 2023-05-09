70567cd7dc6ac7915a37f1ccac698197dfc27453
Tomut0 <minat0_@outlook.com>
2023-05-10 01:09:31 +0300

    remove unused imports

--- src/main/java/me/roinujnosde/titansbattle/BaseGame.java
+++ src/main/java/me/roinujnosde/titansbattle/BaseGame.java
@@ -24,13 +24,11 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import sun.jvm.hotspot.utilities.IntArray;
 
 import java.text.MessageFormat;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static me.roinujnosde.titansbattle.BaseGameConfiguration.Prize;
@@ -549,8 +547,8 @@ public abstract class BaseGame {
             List<Group> groups = warriors.stream().map(Warrior::getGroup).distinct().collect(Collectors.toList());
 
             for (int i = 0; i < groups.size(); i++) {
-                Set<Warrior> warriorsInGroup = Objects.requireNonNull(plugin.getGroupManager()).getWarriors(groups.get(i));
-                teleport(warriorsInGroup, arenaEntrances.get(i % arenaEntrances.size()));
+                Set<Warrior> groupWarriors = Objects.requireNonNull(plugin.getGroupManager()).getWarriors(groups.get(i));
+                teleport(groupWarriors, arenaEntrances.get(i % arenaEntrances.size()));
             }
         } else {
             for (int i = 0; i < warriors.size(); i++) {
