7f01e20dbf5422b248c599be88a7e2a7603a26f4
RoinujNosde <edsonpassosjr@outlook.com>
2023-10-05 20:01:13 -0300

    fix: spacing in item's name (death message)

--- src/main/java/me/roinujnosde/titansbattle/BaseGame.java
+++ src/main/java/me/roinujnosde/titansbattle/BaseGame.java
@@ -575,7 +575,7 @@ public abstract class BaseGame {
                 if (itemMeta != null && itemMeta.hasDisplayName()) {
                     weaponName = itemMeta.getDisplayName();
                 } else {
-                    weaponName = itemInHand.getType().name().replace("_", "").toLowerCase();
+                    weaponName = itemInHand.getType().name().replace("_", " ").toLowerCase();
                 }
             }
             broadcastKey("killed_by", victim.getName(), killsCount.getOrDefault(victim, 0), killer.getName(), killsCount.get(killer), weaponName);
